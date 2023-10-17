d4d2f06801548c9f2f601e6e6bf70ef39b0bb8fa
JulienR66 <julien-rodriguez66@outlook.fr>
2023-10-17 02:02:37 +0200

    modif fonction plus simple

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,18 @@ const slides = [
 	{
 		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
-		"id": 0
 	},
 	{
 		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-		"id": 1
 	},
 	{
 		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-		"id": 2
 	},
 	{
 		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
-		"id": 3
 	}
 ]
 
@@ -39,13 +35,17 @@ flecheDroite.addEventListener('click', () => {
 function mettreAJourCarousel(direction) {
 
 	if (direction === 'droite') {
-		indiceActuel = (indiceActuel + 1) % slides.length;
+		indiceActuel = (indiceActuel + 1);
 	}else if (direction === 'gauche') {
-		indiceActuel = (indiceActuel - 1 + slides.length) % slides.length;
+		indiceActuel = (indiceActuel - 1);
 	}
 
 	if (indiceActuel < 0) {
 		indiceActuel = slides.length - 1;
+	}
+
+	if (indiceActuel > slides.length) {
+		indiceActuel = 0;
 	}
 
 	const imageCarousel = document.querySelector(".banner-img");
